6220eac0d450c6965541cc0cea3ac0cdc8ec64c2
ondrados <ondrejdoskocil@gmail.com>
2022-09-03 16:23:41 +0200

    embed redis-server

--- src/App.tsx
+++ src/App.tsx
@@ -18,11 +18,34 @@ const App: Component = () => {
     alert(result)
   }
 
+  async function setKey(event){
+
+    // Invoke the command from our backend using the invoke function
+    const result = await invoke("set_key", {
+      key: "my-key",
+      value: 99
+    })
+
+  }
+
+  async function getKey(event){
+
+    // Invoke the command from our backend using the invoke function
+    const result = await invoke("get_key", {
+      key: "my-key"
+    })
+
+    // print the result to an alert
+    alert(result)
+  }
+
   return (
     <>
       <div>
         <h1 class="header">{counter()}</h1>
         <button onClick={handleClick}>Click Me</button>
+        <button onClick={setKey}>Set key</button>
+        <button onClick={getKey}>Get key</button>
       </div>
       <Counter />
     </>
